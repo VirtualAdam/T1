79e2d1f33d8f1ecef67b08ff7372cb2b82da5df2
adam <adam@config.com>
2018-11-30 20:04:20 +0000

    power control added to yeti branch. new blueprint stated called bar

--- app/__init__.py
+++ app/__init__.py
@@ -39,6 +39,9 @@ def create_app(config_class=Config):
 
     from app.errors import bp as errors_bp
     app.register_blueprint(errors_bp)
+    
+    from app.bar import bp as bar_bp
+    app.register_blueprint(bar_bp)
 
     from app.auth import bp as auth_bp
     app.register_blueprint(auth_bp, url_prefix='/auth')
